97edace475f487bfbca59a50d6745cfa830d8947
obryadov <obryadov111@mail.ru>
2022-09-23 21:57:55 +0500

    preloading has added

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const { app, BrowserWindow } = require('electron');
 const { head } = require('got');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true
+
         }
     })
     // win.removeMenu();
