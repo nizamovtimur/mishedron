05c223635937095b1bdc458f15bcc710842989f1
Дмитрий Шушарин <stud0000240024@study.utmn.ru>
2022-09-24 12:50:15 +0300

    preloader -done

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+document.body.onload = function (){
+    setTimeout(function (){
+        let preloader = document.getElementById("page-preloader")
+        if(!preloader.classList.contains("done"))
+        {
+            preloader.classList.add("done");
+        }
+    }, 1000)
+}
